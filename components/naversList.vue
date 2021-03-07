4874dcbe32814a1cf948f2cdcb509f78e69c1414
andrews <andrews@wdhouse.com.br>
2021-03-06 22:28:13 -0300

    Rm: modal broken

--- components/naversList.vue
+++ components/naversList.vue
@@ -9,34 +9,17 @@
                     <div v-html="naver.name"></div>
                     <div v-html="naver.job_role"></div>
                     <div class="d-flex">
-                        <img src="/Delete-Icon.svg" alt="delete" title="Delete" @click="showDeletenaver()">
+                        <img src="/Delete-Icon.svg" alt="delete" title="Delete">
                         <img src="/Edit-Icon.svg" alt="edit" title="Edit">
                     </div>
                 </div>
             </div>
         </div>
-        <modal name="deleteNaver">
-            <div class="container">
-                <div> Excluir Naver </div>
-                <div> Tem certeza que deseja excluir esse Naver? </div>
-                <div class="d-flex justify-content-around">
-                    <button class="button action pl-4 pr-4" @click="closeModal()"> Cancelar </button>
-                    <button class="button action pl-4 pr-4"> Excluir </button>
-                </div>
-            </div>
-        </modal>
     </div>
 </template>
 <script>
 export default {
     props: ['navers'],
-    methods:{
-        showDeletenaver(){
-            this.$modal.show('deleteNaver')
-        },
-        closeModal(){
-            this.$modal.hide('deleteNaver')
-        }
-    },
+    
 }
 </script>
