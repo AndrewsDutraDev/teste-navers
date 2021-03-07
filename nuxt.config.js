ec0edb0f9af0749154bba931fa951af3aae3f62d
andrews <andrews@wdhouse.com.br>
2021-03-06 22:48:04 -0300

    Removed unneed plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,7 @@ export default {
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
 		{ src: "~/plugins/vee-validate.js", ssr: true },
-		{src: `~/plugins/axios.js`, ssr: true},
-		{src: `~/plugins/vue-modal.js`, ssr: true},
-	
+		{src: `~/plugins/axios.js`, ssr: true},	
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
